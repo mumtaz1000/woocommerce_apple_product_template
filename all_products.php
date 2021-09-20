7fb0cd6dbca3ef18c6615ead9197c78676ebaf3b
Mumtaz <shereenfatima1000@gmail.com>
2021-09-20 12:18:25 +0200

    Creating single product page and product archive

---
+++ all_products.php
@@ -0,0 +1,50 @@
+  
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Mumtaz Fatima">
+    <title></title>
+    <!-- Normalize cdn link -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+    />
+    
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,900|Source+Sans+Pro:300,900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+<body>
+    <!-- Product Card 1 -->
+<section class="product-card-body">
+  <section class="product-card" id="product-card-section">
+    <div class="product-card-container">
+      
+    <div class="card">
+        <div class="circle"></div>
+          <div class="content">
+             <h3>Response Voodoo Webpage</h3>
+              <a href="https://mumtaz1000.github.io/Medieinstitutet_WIE20S_FEKURS_UPPGIFT1/Uppgift1/Code/index.html" target="_blank">Show Project</a>
+          </div>
+        </div> 
+
+        <div class="card">
+        <div class="circle"></div>
+          <div class="content">
+             <h3>Response Voodoo Webpage</h3>
+              <a href="https://mumtaz1000.github.io/Medieinstitutet_WIE20S_FEKURS_UPPGIFT1/Uppgift1/Code/index.html" target="_blank">Show Project</a>
+          </div>
+        </div> 
+    
+    </div>
+  </section>
+</section>
+    <!-- Footer -->
+    
+</body>
+</html>
---
+++ single_product.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Mumtaz Fatima">
+    <title></title>
+    <!-- Normalize cdn link -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+    />
+   
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,900|Source+Sans+Pro:300,900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+<body>
+    <section class="single-product-body">
+        <div class="product-container-grid">
+        <div class="item1">
+        </div>
+        <div class="item2">
+            <h3 style="color:#E3B091; font-weight:bold;"> Nyhet </h3>
+            <h2 style="font-weight:normal;">Förbeställ</h2>
+            <h2 style="font-weight:lighter;">iPhone 13 Pro </h2>
+            <p>Kommer 24 september.</p>
+            <p>Få 10kr - 20kr rabatt när du byter in en iPhone 10 eller nyare***</p>
+            <p>Ta reda på hur Apple Trade In fungerar</p>
+            <h3>Välj modell.</h3>
+            <p>Vilken modell passar dig bäst?</p>
+            <div class="product-model-div">
+                <h4>iPhone 13 Pro</h4>
+                6,1-tumsskärm
+                Från 12 295 kr
+            </div>
+            <div class="product-model-div">
+                <h4>iPhone 13 Pro Max</h4>
+                6,1-tumsskärm
+                Från 12 295 kr
+            </div>
+            <br>
+            <h3>Välj modell.</h3>
+            <div class="product-finish-div">
+            <BR>
+            <span class="dot" style="background-color:#92A8C0"></span>
+            <span class="dot" style="background-color:#F4E0C7"></span>
+            <span class="dot" style="background-color:#ECEDE7"></span>
+            </div>
+                
+            </div>
+        </div>
+        </div>
+    </section>
+</body>
+</html>
